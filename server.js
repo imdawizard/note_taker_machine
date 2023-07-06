f974a82718dc079c87482f647f75fd4eaa5bfe4e
Spyrodragonman Mannyman <nate.is.cool.yeah@gmail.com>
2023-07-06 14:18:06 -0600

    added post request for server.js. Is currently not working correctly

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
 const express = require("express");
 const app = express();
+const notes = [];
 
 //serve static files from the public directory
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 
 //creates route for GET/notes that returns notes.html
-app.get('/notes', (req, res) => {{
+app.get('/api/notes', (req, res) => {{
     res.sendFile(__dirname + '/public/notes.html');
 }});
 
 //all other routes
 app.get("*", (req, res) => {
     res.sendFile(__dirname + '/public/404.html');
+});
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+
+
+    //pushes new note to notes array
+    notes.push(newNote);
+    //confirms note was saved successsully
+    res.status(200).json({ message: "Note saved successfully"});
+})
+
+app.get('/api/notes', (req, res) => {
+    res.status(200).json(notes);
 });
 
 //listens on port 3001 and logs confirmation
