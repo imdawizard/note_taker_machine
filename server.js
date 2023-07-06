a6245e6b464b4a2446dc684fcbe5ee4ce861e5a9
Spyrodragonman Mannyman <nate.is.cool.yeah@gmail.com>
2023-07-06 17:37:20 -0600

    modified server.js to be able to read and write to db.json file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const notes = [];
+const fs = require('fs');
 
 //serve static files from the public directory
 app.use(express.static('public'));
@@ -20,19 +20,39 @@ app.get("*", (req, res) => {
 app.post('/api/notes', (req, res) => {
     const newNote = req.body;
 
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Failed to read notes from the database'});
+        }
 
-    //pushes new note to notes array
-    notes.push(newNote);
-    //confirms note was saved successsully
-    res.status(200).json({ message: "Note saved successfully"});
-})
+        let notes = JSON.parse(data);
 
-app.get('/api/notes', (req, res) => {
-    res.status(200).json(notes);
+        notes.push(newNote);
+
+        fs.writeFile('db.json', JSON.stringify(notes), (err) =>{
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ error: 'Failed to serve note to the database'});
+            }
+
+            res.status(200).json({ message: "not saved successssfully"});
+        });
+    });
 });
 
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Failed to read notes from database :('})
+        }
+        const notes = JSON.parse(data);
+    res.status(200).json(notes);
+    });
+});
 //listens on port 3001 and logs confirmation
 const port = 3001;
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port} 🚀`)
-})
+});
